e09f55784e5f34e9c4046ceffc1e7944179d179d
mic <micmor.m@gmail.com>
2021-02-19 15:16:55 -0500

    add some style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
+//import Jumbotron from "react-bootstrap/Jumbotron";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 import ChartWrapper from "./ChartWrapper";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ChartWrapper />
+        <Navbar bg="light">
+          <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+        </Navbar>
+        <Container>
+          <ChartWrapper />
+        </Container>
       </div>
     );
   }
--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,8 +1,9 @@
 import * as d3 from "d3";
 
-const data = [20, 12, 16, 25, 20];
+//const data = [20, 12, 16, 25, 20];
 
-const url = "https://udemy-react-d3.firebaseio.com/ages.json";
+//const url = "https://udemy-react-d3.firebaseio.com/ages.json";
+const url = "https://udemy-react-d3.firebaseio.com/tallest_men.json";
 //const url = "../server/dataFiles/data.json";
 
 // d3.json("../server/dataFiles/data.json", function (data) {
@@ -18,35 +19,35 @@ export default class D3Chart {
       .attr("width", 500)
       .attr("height", 500);
 
-    d3.json(url).then((agesData) => {
+    d3.json(url).then((data) => {
       // console.log(agesData);
-      const rects = svg.selectAll("rect").data(agesData);
+      const rects = svg.selectAll("rect").data(data);
       rects
         .enter()
         .append("rect")
         .attr("x", (d, i) => i * 100)
-        .attr("y", 50)
+        .attr("y", 0)
         .attr("width", 50)
-        .attr("height", (d) => d.age * 10)
-        .attr("fill", (d) => {
-          if (d.age > 10) {
-            return "red";
-          }
-          return "green";
-        });
+        .attr("height", (d) => d.height)
+        .attr("fill", "gray");
     });
 
-    // d3.json("../server/dataFiles/data.json").then((agesData) => {
-    //   console.log(agesData);
+    // d3.json(url).then((data) => {
+    //   // console.log(agesData);
+    //   const rects = svg.selectAll("rect").data(data);
+    //   rects
+    //     .enter()
+    //     .append("rect")
+    //     .attr("x", (d, i) => i * 100)
+    //     .attr("y", 50)
+    //     .attr("width", 50)
+    //     .attr("height", (d) => d.age * 10)
+    //     .attr("fill", (d) => {
+    //       if (d.age > 10) {
+    //         return "red";
+    //       }
+    //       return "green";
+    //     });
     // });
-
-    //ONLY one rect
-    // svg
-    //   .append("rect")
-    //   .attr("x", 50)
-    //   .attr("y", 50)
-    //   .attr("width", 100)
-    //   .attr("height", 400)
-    //   .attr("fill", "gray");
   }
 }
