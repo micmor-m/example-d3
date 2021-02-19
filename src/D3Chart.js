41ff6974107b41ca0554d61c3d1018677672f7a9
mic <micmor.m@gmail.com>
2021-02-19 16:02:53 -0500

    align bards down

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -6,6 +6,9 @@ import * as d3 from "d3";
 const url = "https://udemy-react-d3.firebaseio.com/tallest_men.json";
 //const url = "../server/dataFiles/data.json";
 
+const WIDTH = 800;
+const HEIGHT = 500;
+
 // d3.json("../server/dataFiles/data.json", function (data) {
 //   console.log(data);
 // });
@@ -16,18 +19,22 @@ export default class D3Chart {
     const svg = d3
       .select(element)
       .append("svg")
-      .attr("width", 800)
-      .attr("height", 500);
+      .attr("width", WIDTH)
+      .attr("height", HEIGHT);
 
     d3.json(url).then((data) => {
       // console.log(agesData);
 
-      const y = d3.scaleLinear().domain([0, 272]).range([0, 500]);
+      const max = d3.max(data, (d) => {
+        return d.height;
+      });
+
+      const y = d3.scaleLinear().domain([0, max]).range([0, HEIGHT]);
 
       const x = d3
         .scaleBand()
         .domain(data.map((d) => d.name))
-        .range([0, 800])
+        .range([0, WIDTH])
         .padding(0.4);
 
       const rects = svg.selectAll("rect").data(data);
@@ -35,7 +42,7 @@ export default class D3Chart {
         .enter()
         .append("rect")
         .attr("x", (d) => x(d.name))
-        .attr("y", 0)
+        .attr("y", (d) => HEIGHT - y(d.height)) //to make the bars start form botton
         .attr("width", x.bandwidth)
         //.attr("height", (d) => d.height)
         .attr("height", (d) => y(d.height))
